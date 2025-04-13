7b5b8804242a1bb740f80b4e1e7bb27ff0a4a654
Rohan Godha <git@rohangodha.com>
2025-04-13 16:34:27 -0400

    init frontend

---
+++ frontend/vite.config.js
@@ -0,0 +1,33 @@
+import { resolve } from 'node:path'
+import { defineConfig } from 'vite'
+import viteReact from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    TanStackRouterVite({ autoCodeSplitting: true }),
+    viteReact(),
+    tailwindcss(),
+  ],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:8000', // Django dev server
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+})
